1d4ebb2205720f30dd1b3d8b22264a8602533b3d
Javier Bullrich <javier.bullrich@innerspace.eu>
2020-03-04 13:37:10 +0100

    Added base unity project

---
+++ Assets/Scripts/MovingPlatform.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class MovingPlatform : MonoBehaviour
+{
+    [SerializeField] private float speed;
+
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+            transform.position += Vector3.up * speed;
+    }
+}
