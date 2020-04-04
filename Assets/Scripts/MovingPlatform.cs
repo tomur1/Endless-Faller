6d3a7cefe50facf9753e230182f448623afe8570
tomur <tomek.banasiak@gazeta.pl>
2020-04-04 11:42:26 +0200

    Made the platform dissapear when no longer visilble. Close #3

--- Assets/Scripts/MovingPlatform.cs
+++ Assets/Scripts/MovingPlatform.cs
@@ -2,18 +2,29 @@
 
 public class MovingPlatform : MonoBehaviour
 {
+    private GameObject topBorder;
     [SerializeField] private float speed;
     public bool beenHit { get; private set; }
+    //Platforms are visible longer than the player so we have to destroy them later.
+    //How much later is controlled by this offset
+    public float offsetY;
 
     void Start()
     {
         beenHit = false;
+        //Not the mose efficient but it's called only one per object. Also this game doesn't have a lot of objects
+        topBorder = GameObject.Find("TopBorder");
     }
 
     // Update is called once per frame
     void Update()
     {
-            transform.position += Vector3.up * speed;
+        transform.position += Vector3.up * speed;
+
+        if (topBorder.transform.position.y + offsetY < transform.position.y)
+        {
+            Destroy(gameObject);
+        }
     }
 
     public void onHit()
